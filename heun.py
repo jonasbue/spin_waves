c9d41cdcd6eb3867382cc54e40c7bcc601674718
Jonas Bueie <jonasbue@stud.ntnu.no>
2021-03-28 11:44:10 +0200

    Reworks time step and indexing. Solver now accepsts an arbitrary amount of particles.

--- heun.py
+++ heun.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 # Maybe add scipy sparse at some point?
 
 class EquationParameters:
-    J       = 0.0   # Coupling constant between neighboring spins.
+    J       = 1.0   # Coupling constant between neighboring spins.
                     # Would be nice to genaralize this to a function
                     # to allow for long distance coupling, 
                     # but that is really unnecessary for now.
@@ -14,7 +14,7 @@ class EquationParameters:
     gamma   = 1.0   # Gyromagnetic ratio.
     alpha   = 1.0   # Damping constant.
     B       = np.array([0,0,1.0])   # External magnetiic field.
-    ksi     = np.array([0,0,0])     # Thermal noise.
+    ksi     = np.array([1,0,0])     # Thermal noise.
                                     # Will probably stay zero.
 
 def heun(n, t_max, h, params):
@@ -32,8 +32,8 @@ def heun(n, t_max, h, params):
     N = int(t_max//h)
     S = np.zeros((N, n, 3))
     S[0,0,1] = 0.1
-    print("Initial:")
-    print(S)
+    #print("Initial:")
+    #print(S)
     for i in range(N-1):
         S[i+1,:,:] = heun_step(S[i,:,:], h, params)
     return S
@@ -45,11 +45,35 @@ def heun_step(S, h, params):
 
 def time_step(S, params):
     C = params.gamma / (params.mu * (1 + params.alpha**2))
-    Sj_cross_H = (
-        2*params.J*np.cross(S[:,:], S[:,1:] + S[:,:-1])
-        + 2*S[:,2]*np.cross(S[:,:], np.array([0,0,1]))
-        + params.mu*np.cross(S[:,:], params.B)
+    #print(S[1:-1].shape,"\n", S[2:].shape, "\n", S[:-2].shape)
+    #print(2*params.J*np.cross(S[1:-1,:], S[1:,:] + S[:-1,:]))
+    #print("S:")
+    #print(S)
+
+    #print("S[1:-1]\n", S[1:-1,:],"\n S[0:-2]", S[0:-2,:], "\n S[2:]", S[2:,:])
+    #print(np.tile([0,0,1], (len(S)-2,1)))
+
+    Sj_cross_H = np.zeros(S.shape)
+    Sz = np.transpose(np.tile(S[:,2], (3,1)))
+    print(Sz)
+
+    # This is the "main body" of the equation of motion,
+    # containing everythin but the correlation 
+    # between different spins/particles.
+    Sj_cross_H = (2*Sz[:,:]*np.cross(S[:,:], np.tile([0,0,1], (len(S),1)))
+        + params.mu*np.cross(S[:,:], np.tile(params.B, (len(S), 1)))
         - np.cross(S[:,:], params.ksi))
+    # Next we add a correlation term.
+    # The end points only depend on one other point, so we handle
+    # the correlation term separately for those.
+    # If n = 1 or J = 0, then there is not correlation. 
+    if len(S) > 1:
+        Sj_cross_H[0] += 2*params.J*np.cross(S[0,:], S[1,:])
+        Sj_cross_H[-1] += 2*params.J*np.cross(S[-1,:], S[-2,:])
+        # Lastly, we add de correlation term for the remaining points.
+        if len(S) >= 3:
+            Sj_cross_H[1:-1] += 2*params.J*(np.cross(S[1:-1,:], S[0:-2,:] + S[2:,:]))
+
     return C * Sj_cross_H + params.alpha * np.cross(S[:,:], Sj_cross_H)
 
 ## Unnecessary stuff, right?
--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from heun import *
 
 params = EquationParameters()
 
-t_max = 5
+n = 1
+t_max = 1
 h = 0.01
 N = t_max//h
-S = heun(1, t_max, h, params)
+S = heun(n, t_max, h, params)
 plt.plot(np.arange(N), S[:,:,0], label="x")
 plt.plot(np.arange(N), S[:,:,1], label="y")
 plt.plot(np.arange(N), S[:,:,2], label="z")
 plt.legend()
 plt.show()
 
-print("Final:")
-print(S)
+#print("Final:")
+#print(S)
 
 
